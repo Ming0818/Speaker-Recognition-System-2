b70bcd859d35ca9124ceb69c34d492b4aba60246
shefali.msr <201551095@iiitvadodara.ac.in>
2017-12-02 10:15:33 +0530

    first commit

---
+++ train.py
@@ -0,0 +1,23 @@
+from __future__ import division
+import numpy as np
+from scipy.io.wavfile import read
+from vqlbg import lbg
+from mfcc import mfcc
+import pandas as pd 
+
+def training(nfiltbank):
+	nSpeaker=1
+	nCentroid = 16
+	codebooks_mfcc = np.empty((nSpeaker,nfiltbank,nCentroid))
+	directory = 'train'
+	fname = str()
+
+	
+	fname = '/shefali1.wav'
+	(fs,s) = read(directory + fname)
+	mel_coeff = mfcc(s, fs, nfiltbank)
+	codebook = lbg(mel_coeff, nCentroid)
+		
+	 
+	print('Training complete')
+	return (codebook)
